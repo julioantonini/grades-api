54cd711cc7e6179d5e4d06581ab9eac712df9297
Julio <julio.rosa@triasoftware.com.br>
2022-02-09 19:18:28 -0300

    chore: add validation lib

--- apps/grades-api-admin/src/main.ts
+++ apps/grades-api-admin/src/main.ts
@@ -1,8 +1,10 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { GradesApiAdminModule } from './grades-api-admin.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(GradesApiAdminModule);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(3001);
 }
 bootstrap();
