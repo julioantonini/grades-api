fc6f2dd1598ab5a9f173083e1963fb1dd463cbc8
Julio <julio.rosa@triasoftware.com.br>
2022-02-09 19:52:04 -0300

    test: ensure grades service createOrUpdate to be called with correct params

--- apps/grades-api-admin/src/grades/grades.controller.spec.ts
+++ apps/grades-api-admin/src/grades/grades.controller.spec.ts
@@ -1,14 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GradesController } from './grades.controller';
+import { GradesDto } from './grades.dto';
 import { GradesService } from './grades.service';
 
 describe('GradesController', () => {
   let controller: GradesController;
 
+  const gradesServiceMock = {
+    createOrUpdate: jest.fn(),
+  };
+
+  const makeParams = (): GradesDto => ({
+    studentId: 1,
+    studentName: 'any name',
+    n1: 1,
+    n2: 2,
+    n3: 3,
+    n4: 4,
+  });
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [GradesController],
-      providers: [GradesService],
+      providers: [
+        {
+          provide: GradesService,
+          useValue: gradesServiceMock,
+        },
+      ],
     }).compile();
 
     controller = module.get<GradesController>(GradesController);
@@ -16,5 +35,11 @@ describe('GradesController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should call GradesService createOrUpdate with correct params', async () => {
+    const params = makeParams();
+    await controller.createOrUpdate(params);
+    expect(gradesServiceMock.createOrUpdate).toHaveBeenCalledWith(params);
   });
 });
