800eba0af5f296ec16b330c55ced3d021ca62783
Julio <julio.rosa@triasoftware.com.br>
2022-02-09 22:13:04 -0300

    chore: bootstrap swagger

--- apps/grades-api-admin/src/main.ts
+++ apps/grades-api-admin/src/main.ts
@@ -1,10 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { GradesApiAdminModule } from './grades-api-admin.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(GradesApiAdminModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Grades api admin')
+    .setDescription('Api for registration and listing students and grades')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3001);
 }
 bootstrap();
