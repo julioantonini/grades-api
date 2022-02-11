f097236628a7acfca979d88bd1400f668f617bba
Julio <julio.rosa@triasoftware.com.br>
2022-02-10 22:28:18 -0300

    feat: logger lib

--- libs/logger/src/logger.service.ts
+++ libs/logger/src/logger.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { LOGGER_CONTEXT } from './types/logger-context.enum';
 
 @Injectable()
-export class LoggerService {}
+export class LoggerService {
+  private context: LOGGER_CONTEXT;
+
+  public setContext(context: LOGGER_CONTEXT): void {
+    this.context = context;
+  }
+
+  public log(message: string): void {
+    console.log(`[${this.context}] ${new Date()} ${message}`);
+  }
+
+  public logError(message: string, error: any): void {
+    console.log(`[${this.context}] ${new Date()} ${message}: ${error}`);
+  }
+}
