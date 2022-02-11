97b7a50dbace9da2fca1e6d511793be1f3f6f5d2
Julio <julio.rosa@triasoftware.com.br>
2022-02-11 09:04:19 -0300

    fix: fix tests

--- apps/grades-api-admin/src/grades/grades.controller.spec.ts
+++ apps/grades-api-admin/src/grades/grades.controller.spec.ts
@@ -1,3 +1,4 @@
+import { LoggerService, LOGGER_CONTEXT } from '@logger/logger';
 import { Test, TestingModule } from '@nestjs/testing';
 import { GradesController } from './grades.controller';
 import { GradesService } from './grades.service';
@@ -6,6 +7,7 @@ import { makeGradesParamsMock } from './mock/make-grades-params.mock';
 describe('GradesController', () => {
   let sut: GradesController;
   let service: GradesService;
+  let loggerService: LoggerService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -18,11 +20,20 @@ describe('GradesController', () => {
             findAll: jest.fn(),
           },
         },
+        {
+          provide: LoggerService,
+          useValue: {
+            setContext: jest.fn(),
+            log: jest.fn(),
+            logError: jest.fn(),
+          },
+        },
       ],
     }).compile();
 
     sut = module.get<GradesController>(GradesController);
     service = module.get<GradesService>(GradesService);
+    loggerService = module.get<LoggerService>(LoggerService);
   });
 
   it('should be defined', () => {
@@ -33,10 +44,18 @@ describe('GradesController', () => {
     const params = makeGradesParamsMock();
     await sut.createOrUpdate(params);
     expect(service.createOrUpdate).toHaveBeenCalledWith(params);
+    expect(loggerService.setContext).toHaveBeenCalledWith(
+      LOGGER_CONTEXT.GRADES_API_ADMIN,
+    );
+    expect(loggerService.log).toHaveBeenCalledTimes(1);
   });
 
   it('should call GradesService findAll', async () => {
     await sut.findAll();
     expect(service.findAll).toHaveBeenCalled();
+    expect(loggerService.setContext).toHaveBeenCalledWith(
+      LOGGER_CONTEXT.GRADES_API_ADMIN,
+    );
+    expect(loggerService.log).toHaveBeenCalledTimes(1);
   });
 });
